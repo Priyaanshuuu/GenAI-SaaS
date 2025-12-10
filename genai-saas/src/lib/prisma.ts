2d716ae0e31032c8a261fc8879f3c3950a9c36d1
Priyanshu Sinha <PriyanshuSinha636@gmail.com>
2025-12-10 18:01:00 +0530

    prismaClient

--- genai-saas/src/lib/prisma.ts
+++ genai-saas/src/lib/prisma.ts
@@ -1,3 +1,7 @@
+// prismaClient is nothing but a way through which we can communicate to the database
+// below code is something where we have creeated our prismaClient as a global variable so that we can use it from anywhere
+
+
 import { PrismaClient } from "@prisma/client/extension";
 
 const globalForPrisma = global  as unknown as {prisma: PrismaClient};
